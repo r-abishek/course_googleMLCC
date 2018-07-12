1fd14314f6d7fdcde50477ea71db9950b3b16907
r-abishek <abishek.genex@gmail.com>
2018-07-11 17:59:14 -0700

    10_logistic_regression

--- py/10_logistic_regression.py
+++ py/10_logistic_regression.py
@@ -49,7 +49,7 @@ def preprocess_features(california_housing_dataframe):
     processed_features = selected_features.copy()
 
     # Create synthetic feature
-    processed_features = ["rooms_per_person"] = (
+    processed_features["rooms_per_person"] = (
         california_housing_dataframe["total_rooms"] / 
         california_housing_dataframe["population"]
     )
@@ -91,3 +91,308 @@ print("Validation Examples Summary:")
 print(validation_examples.describe())
 print("Validation Targets Summary:")
 print(validation_targets.describe())
+
+# Try Linear Regression
+def construct_feature_columns(input_features):
+    """
+    Construct the TensorFlow Feature Columns.
+    Args:
+        input_features: The names of the numerical input features to use.
+    Returns:
+        A set of feature columns
+    """
+    return set([tf.feature_column.numeric_column(my_feature) for my_feature in input_features])
+
+def my_input_fn(features, targets, batch_size = 1, shuffle = True, num_epochs = None):
+    """
+    Trains a linear regression model.
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+
+    # Convert pandas data into a dict of np arrays.
+    features = {key:np.array(value) for key,value in dict(features).items()}                                            
+ 
+    # Construct a dataset, and configure batching/repeating.
+    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    
+    # Shuffle the data, if specified.
+    if shuffle:
+      ds = ds.shuffle(10000)
+    
+    # Return the next batch of data.
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+def train_linear_regressor_model(
+    learning_rate,
+    steps,
+    batch_size,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+    """
+    Trains a linear regression model.
+    In addition to training, this function also prints training progress information,
+    as well as a plot of the training and validation loss over time.
+    
+    Args:
+        learning_rate: A `float`, the learning rate.
+        steps: A non-zero `int`, the total number of training steps. A training step
+        consists of a forward and backward pass using a single batch.
+        batch_size: A non-zero `int`, the batch size.
+        training_examples: A `DataFrame` containing one or more columns from
+        `california_housing_dataframe` to use as input features for training.
+        training_targets: A `DataFrame` containing exactly one column from
+        `california_housing_dataframe` to use as target for training.
+        validation_examples: A `DataFrame` containing one or more columns from
+        `california_housing_dataframe` to use as input features for validation.
+        validation_targets: A `DataFrame` containing exactly one column from
+        `california_housing_dataframe` to use as target for validation.
+        
+    Returns:
+        A `LinearRegressor` object trained on the training data.
+    """
+
+    periods = 10
+    steps_per_period = steps / periods
+
+    # Create a linear regressor object.
+    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+    linear_regressor = tf.estimator.LinearRegressor(
+        feature_columns=construct_feature_columns(training_examples),
+        optimizer=my_optimizer
+    )
+        
+    # Create input functions.
+    training_input_fn = lambda: my_input_fn(
+        training_examples,
+        training_targets["median_house_value_is_high"],
+        batch_size=batch_size
+    )
+    predict_training_input_fn = lambda: my_input_fn(
+        training_examples,
+        training_targets["median_house_value_is_high"],
+        num_epochs=1,
+        shuffle=False
+    )
+    predict_validation_input_fn = lambda: my_input_fn(
+        validation_examples,
+        validation_targets["median_house_value_is_high"],
+        num_epochs=1,
+        shuffle=False
+    )
+
+    # Train the model, but do so inside a loop so that we can periodically assess
+    # loss metrics.
+    print("Training model...")
+    print("RMSE (on training data):")
+    training_rmse = []
+    validation_rmse = []
+    for period in range (0, periods):
+        # Train the model, starting from the prior state.
+        linear_regressor.train(
+            input_fn=training_input_fn,
+            steps=steps_per_period
+        )
+        
+        # Take a break and compute predictions.
+        training_predictions = linear_regressor.predict(input_fn=predict_training_input_fn)
+        training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+        
+        validation_predictions = linear_regressor.predict(input_fn=predict_validation_input_fn)
+        validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+        
+        # Compute training and validation loss.
+        training_root_mean_squared_error = math.sqrt(
+            metrics.mean_squared_error(training_predictions, training_targets))
+        validation_root_mean_squared_error = math.sqrt(
+            metrics.mean_squared_error(validation_predictions, validation_targets))
+        # Occasionally print the current loss.
+        print("  period %02d : %0.2f" % (period, training_root_mean_squared_error))
+        # Add the loss metrics from this period to our list.
+        training_rmse.append(training_root_mean_squared_error)
+        validation_rmse.append(validation_root_mean_squared_error)
+    print("Model training finished.")
+    
+    # Output a graph of loss metrics over periods.
+    plt.ylabel("RMSE")
+    plt.xlabel("Periods")
+    plt.title("Root Mean Squared Error vs. Periods")
+    plt.tight_layout()
+    plt.plot(training_rmse, label="training")
+    plt.plot(validation_rmse, label="validation")
+    plt.legend()
+    plt.show()
+
+    return linear_regressor
+
+linear_regressor = train_linear_regressor_model(
+    learning_rate=0.000001,
+    steps=200,
+    batch_size=20,
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
+
+predict_validation_input_fn = lambda : my_input_fn(
+    validation_examples,
+    validation_targets["median_house_value_is_high"],
+    num_epochs = 1,
+    shuffle = False
+)
+validation_predictions = linear_regressor.predict(input_fn = predict_validation_input_fn)
+validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+
+_ = plt.hist(validation_predictions)
+plt.show()
+
+def train_linear_classifier_model(
+    learning_rate,
+    steps,
+    batch_size,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+    """
+    Trains a linear classification model.
+    In addition to training, this function also prints training progress information,
+    as well as a plot of the training and validation loss over time.
+    
+    Args:
+        learning_rate: A `float`, the learning rate.
+        steps: A non-zero `int`, the total number of training steps. A training step
+        consists of a forward and backward pass using a single batch.
+        batch_size: A non-zero `int`, the batch size.
+        training_examples: A `DataFrame` containing one or more columns from
+        `california_housing_dataframe` to use as input features for training.
+        training_targets: A `DataFrame` containing exactly one column from
+        `california_housing_dataframe` to use as target for training.
+        validation_examples: A `DataFrame` containing one or more columns from
+        `california_housing_dataframe` to use as input features for validation.
+        validation_targets: A `DataFrame` containing exactly one column from
+        `california_housing_dataframe` to use as target for validation.
+        
+    Returns:
+        A `LinearClassifier` object trained on the training data.
+    """
+    periods = 10
+    steps_per_period = steps / periods
+    
+    # Create a linear classifier object.
+    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+    linear_classifier = tf.estimator.LinearClassifier(
+        feature_columns=construct_feature_columns(training_examples),
+        optimizer=my_optimizer
+    )
+    
+    # Create input functions.
+    training_input_fn = lambda: my_input_fn(
+        training_examples,
+        training_targets["median_house_value_is_high"],
+        batch_size=batch_size
+    )
+    predict_training_input_fn = lambda: my_input_fn(
+        training_examples,
+        training_targets["median_house_value_is_high"],
+        num_epochs=1,
+        shuffle=False
+    )
+    predict_validation_input_fn = lambda: my_input_fn(
+        validation_examples,
+        validation_targets["median_house_value_is_high"],
+        num_epochs=1,
+        shuffle=False
+    )
+    
+    # Train the model, but do so inside a loop so that we can periodically assess
+    # loss metrics.
+    print("Training model...")
+    print("LogLoss (on training data):")
+    training_log_losses = []
+    validation_log_losses = []
+    for period in range (0, periods):
+        # Train the model, starting from the prior state.
+        linear_classifier.train(
+            input_fn=training_input_fn,
+            steps=steps_per_period
+        )
+        # Take a break and compute predictions.    
+        training_probabilities = linear_classifier.predict(input_fn=predict_training_input_fn)
+        training_probabilities = np.array([item['probabilities'] for item in training_probabilities])
+        
+        validation_probabilities = linear_classifier.predict(input_fn=predict_validation_input_fn)
+        validation_probabilities = np.array([item['probabilities'] for item in validation_probabilities])
+        
+        training_log_loss = metrics.log_loss(training_targets, training_probabilities)
+        validation_log_loss = metrics.log_loss(validation_targets, validation_probabilities)
+        # Occasionally print the current loss.
+        print("  period %02d : %0.2f" % (period, training_log_loss))
+        # Add the loss metrics from this period to our list.
+        training_log_losses.append(training_log_loss)
+        validation_log_losses.append(validation_log_loss)
+    print("Model training finished.")
+    
+    # Output a graph of loss metrics over periods.
+    plt.ylabel("LogLoss")
+    plt.xlabel("Periods")
+    plt.title("LogLoss vs. Periods")
+    plt.tight_layout()
+    plt.plot(training_log_losses, label="training")
+    plt.plot(validation_log_losses, label="validation")
+    plt.legend()
+    plt.show()
+
+    return linear_classifier
+
+linear_classifier = train_linear_classifier_model(
+    learning_rate=0.000005,
+    steps=500,
+    batch_size=20,
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
+
+# Calculate accuracy and plot ROC curve for Validation
+evaluation_metrics = linear_classifier.evaluate(input_fn=predict_validation_input_fn)
+
+print("AUC on the validation set: %0.2f" % evaluation_metrics['auc'])
+print("Accuracy on the validation set: %0.2f" % evaluation_metrics['accuracy'])
+
+validation_probabilities = linear_classifier.predict(input_fn=predict_validation_input_fn)
+# Get just the probabilities for the positive class.
+validation_probabilities = np.array([item['probabilities'][1] for item in validation_probabilities])
+
+false_positive_rate, true_positive_rate, thresholds = metrics.roc_curve(
+    validation_targets, validation_probabilities)
+plt.plot(false_positive_rate, true_positive_rate, label="our model")
+plt.plot([0, 1], [0, 1], label="random classifier")
+_ = plt.legend(loc=2)
+plt.show()
+
+linear_classifier = train_linear_classifier_model(
+    learning_rate=0.000003,
+    steps=20000,
+    batch_size=500,
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
+
+evaluation_metrics = linear_classifier.evaluate(input_fn=predict_validation_input_fn)
+
+print("AUC on the validation set: %0.2f" % evaluation_metrics['auc'])
+print("Accuracy on the validation set: %0.2f" % evaluation_metrics['accuracy'])
